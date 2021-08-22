2c2b26b8c03606ddbc43abc00940ea7701ddcee3
Sprudeel <leonard@schafer.li>
2021-08-22 14:11:13 +0200

    Gui of Menu Finished

---
+++ source.js
@@ -0,0 +1,32 @@
+
+document.getElementById(400).style.display = "none";
+
+// hide Menu
+function hideMenu() {
+    var menu = document.getElementById(99);
+    var animation = [
+        {transform: "translateX(-100%)"}
+    ];
+    var timing = {
+        duration: 500,
+        fill: 'forwards',
+        iterations: 1
+    }
+    menu.animate(animation, timing);
+    setTimeout(function() { document.getElementById(400).style.display = "block"; }, 500);
+}
+
+function expandMenu() {
+    var menu = document.getElementById(99);
+    var animation = [
+        {transform: "translateX(0%)"}
+        
+    ];
+    var timing = {
+        duration: 500,
+        fill: 'forwards',
+        iterations: 1
+    }
+    menu.animate(animation, timing);
+    document.getElementById(400).style.display = "none";
+}
