af4ce5dc067f3ed59776dde72ca22f8e2bf4b015
Sprudeel <79663902+Sprudeel@users.noreply.github.com>
2021-08-25 15:22:01 +0200

    minor changes

--- source.js
+++ source.js
@@ -30,3 +30,33 @@ function expandMenu() {
     menu.animate(animation, timing);
     document.getElementById(400).style.display = "none";
 }
+
+function showCorrect(name) {
+
+    // Create a new Element to host all informations
+
+    // Fill Element
+    
+}
+
+
+
+
+// Multilingual Shit
+var language = {
+    en: {
+        credit: "created by Sprudeel"
+    },
+    de: {
+        credit: "erstellt von Sprudeel"
+    }
+};
+
+
+if (window.location.hash) {
+    if (window.location.hash === "#de") {
+        credit.textContent = language.de.credit;
+    }
+}
+
+
